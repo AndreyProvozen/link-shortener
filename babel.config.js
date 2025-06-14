ecf4f04d06a0191dcc4a5b9a1589c041febc6266
Андрей Провозен <anprovozens@gmail.com>
2025-06-13 18:26:32 -0600

    update imports for hooks

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,7 @@ module.exports = {
       {
         '@/atoms': { transform: '@/atoms/${member}', preventFullImport: true },
         '@/icons': { transform: '@/icons/${member}', preventFullImport: true },
+        '@/hooks': { transform: '@/hooks/${member}', preventFullImport: true },
         // '@/components': { transform: 'src/components/${member}', preventFullImport: true },
         '@/utils': { transform: '@/utils/${member}', preventFullImport: true },
       },
